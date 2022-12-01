7b08459d8537e93551f688ee0eb11314428266f5
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-12-01 07:26:22 -0300

    1 file modified

--- src/apresentacao.js
+++ src/apresentacao.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function apresencao() {
   return (
-    <div class="cabecalho">
+    <div>
       <img src="https://cdn-icons-png.flaticon.com/512/1803/1803671.png"></img>
       <h1>Michael Cardoso de Almeida</h1>
       <p>Seja bem vindo,</p>
