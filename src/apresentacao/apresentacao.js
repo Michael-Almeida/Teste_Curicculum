6705d7670eb317a383656c9172abacb18e12531d
Michael-Almeida <73245553+Michael-Almeida@users.noreply.github.com>
2022-12-06 13:13:36 -0300

    editando

--- src/apresentacao/apresentacao.js
+++ src/apresentacao/apresentacao.js
@@ -4,14 +4,19 @@ import './style.css';
 export default function apresencao() {
   return (
     <div class="principal">
-      <img src="https://cdn-icons-png.flaticon.com/512/1803/1803671.png"></img>
+      <div class="logo">
+        <img
+          class="imagemCapa"
+          src="https://cdn-icons-png.flaticon.com/512/1803/1803671.png"
+        ></img>
+      </div>
       <h1 class="titulo">Michael Cardoso de Almeida</h1>
       <h3>Seja bem vindo,</h3>
       <p>
         Aqui você irá conhecer um pouco mais do Michael Profissional , Hard e
         soft Skils!
       </p>
-      <button class='botaoApresentacao'>Saiba mais</button>
+      <button class="botaoApresentacao">Saiba mais</button>
     </div>
   );
 }
